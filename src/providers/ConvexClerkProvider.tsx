a0c094c1ce5f35e24cb713f1772f21180fa46a87
Ruth Moraa <moraar37@gmail.com>
2025-06-13 04:28:43 +0300

    changing convex

--- src/providers/ConvexClerkProvider.tsx
+++ src/providers/ConvexClerkProvider.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
-import { ConvexReactClient } from "convex/react";
+// import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+// const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 function ConvexClerkProvider({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {/* <ConvexProviderWithClerk client={convex} useAuth={useAuth}> */}
+      <ConvexProviderWithClerk  useAuth={useAuth}>
         {children}
       </ConvexProviderWithClerk>
     </ClerkProvider>
